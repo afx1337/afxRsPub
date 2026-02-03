93fe6d01f934df02e7bf216650350bb467cf79d0
afx1337 <max.wawrzyniak@medizin.uni-leipzig.de>
2026-02-03 23:10:51 +0100

    Update afxMetaData.m

--- scripts/afxMetaData.m
+++ scripts/afxMetaData.m
@@ -1,10 +1,18 @@
 function meta = afxMetaData()
     meta.date = datestr(now);
-    if isfile("VERSION")
-        meta.version = strtrim(fileread("VERSION"));
-    else
-        meta.version = "unknown";
-    end
+    meta.version = afxVersion();
     meta.matlab = version();
     meta.toolboxes = ver();
+end
+
+function version = afxVersion()
+    thisFile = mfilename('fullpath');
+    thisDir = fileparts(thisFile);
+    versionFile = fullfile(thisDir,'..', 'VERSION');
+    if exist(versionFile, 'file')
+        version = strtrim(fileread(versionFile));
+    else
+        version = 'unknown';
+        warning('VERSION file not found.');
+    end
 end
